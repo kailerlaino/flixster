266ae18c482a55f791dd8f65e95529bd3f34e320
Kailer Laino <kailer@meta.com>
2025-06-10 12:20:15 -0700

    begin building out main app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import MovieList from "./components/MovieList";
+import SearchForm from "./components/SearchForm";
+import NavBar from "./components/NavBar";
+import "./App.css";
 
 const App = () => {
+
   return (
     <div className="App">
-    
-    </div>
-  )
-}
+      <header>
+        <h1>Flixster</h1>
+        <NavBar />
+      </header>
 
-export default App
+      <main>
+        <SearchForm />
+        <section className="card-list">
+          <MovieList />
+        </section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export default App;
